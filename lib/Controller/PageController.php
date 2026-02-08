a5598838bcec43105a1c18baf3fa65aeeb42ebe0
Homer Jay Simpson <24945112+89simpson@users.noreply.github.com>
2026-02-09 00:40:46 +0300

    fix: Revert to original getLastEntries code without UTF-8 processing

--- lib/Controller/PageController.php
+++ lib/Controller/PageController.php
@@ -84,13 +84,7 @@ class PageController extends Controller
             return new DataResponse(['error' => $e->getMessage()], Http::STATUS_INTERNAL_SERVER_ERROR);
         }
         $response = array_map(static function ($entry) {
-            // Агрессивная очистка невалидных UTF-8 символов из базы данных
-            $content = $entry->getEntryContent();
-            // Метод 1: удаляем управляющие символы
-            $content = preg_replace('/[\x00-\x08\x0B\x0C\x0E-\x1F\x7F-\x9F]/u', '', $content);
-            // Метод 2: конвертируем и игнорируем невалидные байты
-            $content = iconv('UTF-8', 'UTF-8//IGNORE', $content);
-            return ['date' => $entry->getEntryDate(), 'excerpt' => substr($content, 0, 40)];
+            return ['date' => $entry->getEntryDate(), 'excerpt' => substr($entry->getEntryContent(), 0, 40)];
         }, $entries);
 
         return new DataResponse($response);
