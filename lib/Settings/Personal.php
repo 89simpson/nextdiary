53ac5f81535ecf0b989bd65556c051784a888dff
Homer Jay Simpson <24945112+89simpson@users.noreply.github.com>
2026-02-12 00:37:49 +0300

    fix: Use IUserSession instead of $userId in Personal settings
    Nextcloud DI does not auto-resolve $userId for ISettings classes,
    only for controllers. Use IUserSession->getUser()->getUID() instead.

--- lib/Settings/Personal.php
+++ lib/Settings/Personal.php
@@ -3,22 +3,25 @@ namespace OCA\NextDiary\Settings;
 
 use OCP\AppFramework\Http\TemplateResponse;
 use OCP\IConfig;
+use OCP\IUserSession;
 use OCP\Settings\ISettings;
 
 class Personal implements ISettings {
     private $config;
-    private $userId;
+    private $userSession;
 
-    public function __construct(IConfig $config, string $userId) {
+    public function __construct(IConfig $config, IUserSession $userSession) {
         $this->config = $config;
-        $this->userId = $userId;
+        $this->userSession = $userSession;
     }
 
     public function getForm(): TemplateResponse {
+        $user = $this->userSession->getUser();
+        $userId = $user ? $user->getUID() : '';
         $settings = [];
         $keys = ['show_mood', 'show_wellbeing', 'show_tags', 'show_symptoms', 'show_medications'];
         foreach ($keys as $key) {
-            $settings[$key] = $this->config->getUserValue($this->userId, 'nextdiary', $key, 'true') === 'true';
+            $settings[$key] = $this->config->getUserValue($userId, 'nextdiary', $key, 'true') === 'true';
         }
         return new TemplateResponse('nextdiary', 'personal', ['settings' => $settings]);
     }
